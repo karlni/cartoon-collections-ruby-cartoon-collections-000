140a93344183c7e97811fede35d7b8115c03101f
agent.karl.zl.ni <agent.karl.zl.ni@gmail.com>
2018-11-06 20:05:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,28 +1,6 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do | name, index |
-    puts "#{index + 1} #{name}"
-  end
+def roll_call_dwarves(names)
+  puts names.each_with_index {|name| name}
 end
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
-    call.capitalize + "!"
-  end
-end
-
-def long_planeteer_calls(words)
-  words.any? do |word|
-  word.length > 4
-  end
-end
-
-def find_the_cheese(cheesy)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-   cheesy.find do | type |
-     cheese_types.include?(type)
-    end
-end
-
 
 roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
